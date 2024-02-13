36feb59b074e234e4c55ddd1e4494e86c5c9a688
Abhi... <8083613+AbhimanyuAryan@users.noreply.github.com>
2024-02-13 23:31:33 +0000

    removed slug

--- pages/deep-learning.vue
+++ pages/deep-learning.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>Deep Learning Posts</h1>
+    <p>fuck you nuxt you are horrible. should have used gatsby</p>
     <ul>
       <li v-for="post in posts" :key="post.slug">
         <nuxt-link :to="`/deep-learning/${post.slug}`">
