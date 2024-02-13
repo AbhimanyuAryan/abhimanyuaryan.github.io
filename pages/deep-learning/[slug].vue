752352cd818bda1f5ddf91b2202d0be4e2059af6
Abhi... <8083613+AbhimanyuAryan@users.noreply.github.com>
2024-02-13 23:29:01 +0000

    revert to default

--- pages/deep-learning/[slug].vue
+++ pages/deep-learning/[slug].vue
@@ -1,21 +1,17 @@
-<script setup lang="ts">
-import { onContentNotFound } from '~/utils/content.js';
-import { useRoute } from 'vue-router';
-import { computed } from 'vue';
-
-// Fetching deep learning content based on the slug from the URL
-const { page: deepLearningPost } = useContent('deep-learning/' + useRoute().params.slug);
-
-onContentNotFound(deepLearningPost);
-
-// Formatting the creation date to be human-readable
-const formattedCreatedAt = computed(() => new Date(deepLearningPost.value.datePublished).toLocaleDateString());
-</script>
-
 <template>
-  <div class="deep-learning-post-container">
-    <h1>{{ deepLearningPost.title }}</h1>
-    <p class="published-date">Published at: {{ formattedCreatedAt }}</p>
-    <div v-html="deepLearningPost.bodyHtml"></div>
-  </div>
+  <article>
+    <h1>{{ post.title }}</h1>
+    <div v-html="post.body" />
+  </article>
 </template>
+
+<script setup>
+import { useRoute } from 'vue-router';
+import { useAsyncData } from 'nuxt/app';
+
+const route = useRoute();
+
+const { data: post } = useAsyncData('post', () => {
+  return useContent(`deep-learning/${route.params.slug}`);
+});
+</script>
--- pages/deep-learning/index.vue
+++ pages/deep-learning/index.vue
@@ -1,27 +1,18 @@
-<script setup lang="ts">
-import type { QueryBuilderParams } from '@nuxt/content/dist/runtime/types'
-import { definePageMeta } from 'nuxt/app';
-
-// Specifying the query for fetching deep learning content, sorted by modification and publication dates
-const query: QueryBuilderParams = { path: '/deep-learning', sort: [{ dateModified: -1, datePublished: -1 }] }
-
-// Setting page metadata
-definePageMeta({
-  title: 'Deep Learning Posts',
-  description: 'Explore my deep learning posts, covering a range of topics from fundamentals to advanced techniques.'
-});
-</script>
-
 <template>
-  <section class="deep-learning-posts-list">
+  <section>
     <h1>Deep Learning Posts</h1>
-    <ContentList :query="query" v-slot="{ list }">
-      <div v-for="post in list" :key="post._path" class="deep-learning-post-preview">
-        <h2>{{ post.title }}</h2>
-        <p>{{ post.description }}</p>
-        <!-- Link to the detailed view of the deep learning post -->
-        <nuxt-link :to="`/deep-learning/${post.slug}`">Read more</nuxt-link>
-      </div>
-    </ContentList>
+    <ul>
+      <li v-for="post in posts" :key="post.slug">
+        <NuxtLink :to="`/deep-learning/${post.slug}`">{{ post.title }}</NuxtLink>
+      </li>
+    </ul>
   </section>
 </template>
+
+<script setup>
+import { useAsyncData } from 'nuxt/app';
+
+const { data: posts } = useAsyncData('posts', () => {
+  return useContent('deep-learning', { deep: true }).sortBy('date', 'desc');
+});
+</script>
