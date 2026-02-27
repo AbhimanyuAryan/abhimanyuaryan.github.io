8e9ac78987e258e14660d9d276d206b50021f396
aaryan-cegid <aaryan@cegid.com>
2026-02-27 18:58:40 +0000

    Deploying to gh-pages from @ AbhimanyuAryan/abhimanyuaryan.github.io@065e9f2f0badff4441f88fe9b8c6d2bbd1018ba9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,13 +51,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
               description: "",
@@ -440,16 +433,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+            },},{id: "news-started-masters-in-informatics-engineering-at-university-of-minho",
+          title: 'Started Masters in Informatics Engineering at University of Minho',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-started-as-senior-fullstack-engineer-at-cegid-lisbon-amp-amp-lyon-leading-the-ai-agentic-evals-team-after-shine-s-acquisition",
+          title: 'Started as Senior FullStack Engineer at CEGID (Lisbon &amp;amp;amp; Lyon), leading the AI...',
+          description: "",
+          section: "News",},{id: "news-started-postgraduate-in-marketing-intelligence-at-nova-ims-information-management-school-lisbon-amp-amp-lyon-sparkles",
+          title: 'Started Postgraduate in Marketing Intelligence at NOVA IMS Information Management School (Lisbon &amp;amp;amp;...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
@@ -518,14 +511,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61%62%68%69%6D%61%6E%79%75%61%72%79%61%6E@%70%72%6F%74%6F%6E%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/AbhimanyuAryan", "_blank");
         },
       },{
         id: 'social-rss',
@@ -533,20 +526,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
