5970e08e684c8ffe2964b094445fa8174d9444b0
AbhimanyuAryan <8083613+AbhimanyuAryan@users.noreply.github.com>
2026-04-05 02:42:58 +0000

    Deploying to gh-pages from @ AbhimanyuAryan/abhimanyuaryan.github.io@77a75a2626133937aeb12688a0ede8a4c2ffb1cb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,13 +37,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "Talks and workshops I have given on Julia, machine learning, game development, and more.",
