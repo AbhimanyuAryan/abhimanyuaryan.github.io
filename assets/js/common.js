8eefb5a3b68bd8167c13407a8a130465c21b2ad9
aaryan-cegid <aaryan@cegid.com>
2026-04-12 02:16:42 +0100

    Remove Hacker Theme integration documentation from the repository

--- assets/js/common.js
+++ assets/js/common.js
@@ -43,14 +43,12 @@ $(document).ready(function () {
   $(".jupyter-notebook-iframe-container iframe").each(function () {
     $(this).contents().find("head").append(cssLink);
 
-    if (jupyterTheme == "dark" || jupyterTheme == "hacker") {
-      $(this).bind("load", function () {
-        $(this).contents().find("body").attr({
-          "data-jp-theme-light": "false",
-          "data-jp-theme-name": "JupyterLab Dark",
-        });
+    $(this).bind("load", function () {
+      $(this).contents().find("body").attr({
+        "data-jp-theme-light": "false",
+        "data-jp-theme-name": "JupyterLab Dark",
       });
-    }
+    });
   });
 
   // trigger popovers
