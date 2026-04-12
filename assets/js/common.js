e4fefd7115d624934bb0f4023e73cca538d87b47
aaryan-cegid <aaryan@cegid.com>
2026-04-12 02:21:16 +0100

    Feature/hacker theme exploration (#3)
    * feat: integrate hacker theme with terminal aesthetic, toggle support, and documentation
    
    * feat: update hacker theme styles for improved table and text color consistency
    
    * Remove Hacker Theme integration documentation from the repository
    
    * feat: enhance Jupyter notebook theme loading for immediate dark theme application

--- assets/js/common.js
+++ assets/js/common.js
@@ -43,14 +43,19 @@ $(document).ready(function () {
   $(".jupyter-notebook-iframe-container iframe").each(function () {
     $(this).contents().find("head").append(cssLink);
 
-    if (jupyterTheme == "dark") {
-      $(this).bind("load", function () {
-        $(this).contents().find("body").attr({
-          "data-jp-theme-light": "false",
-          "data-jp-theme-name": "JupyterLab Dark",
-        });
+    // Set dark theme immediately if iframe already loaded
+    $(this).contents().find("body").attr({
+      "data-jp-theme-light": "false",
+      "data-jp-theme-name": "JupyterLab Dark",
+    });
+
+    // Also set on future load events
+    $(this).on("load", function () {
+      $(this).contents().find("body").attr({
+        "data-jp-theme-light": "false",
+        "data-jp-theme-name": "JupyterLab Dark",
       });
-    }
+    });
   });
 
   // trigger popovers
